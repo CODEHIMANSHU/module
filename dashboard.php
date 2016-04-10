020b0d0dafa3071c92f425d10c90d93244374ed5
codehimanshu <himanshuagrawal1998@gmail.com>
2016-04-10 09:10:23 +0530

    dashboard basic structure added

---
+++ dashboard.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>
+      MCQ
+    </title>
+  </head>
+  <body background="img/bg.jpg">
+  <!--Nav bar with gdgjss logo-->
+    <div class="navbar-fixed  ">
+      <nav class="black" style="opacity:0.8;">
+        <div class="nav-wrapper" style="padding: 6px 3px 5px 30px;">
+          <a class="brand-logo"><img style="z-index:+4; opacity:1;" src="img/logo.png" height="50px"></a>
+          <div class="right">
+            <ul>
+              <li><form action="#" name="out" method="POST"><input class="waves-effect waves-light" type="submit" value="Logout" name="out">  </form></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <!--footer-->
+    <div style="position: fixed; height: 30px; width: 100%; bottom: 0px;" class="center grey">
+      <footer>
+        Google Developers Group
+      </footer>
+    </div>
+    <!--Import jQuery before materialize.js-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
+  </body>
+</html>
+ 
